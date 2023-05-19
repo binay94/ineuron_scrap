aded7c16c95d41c3852469335e7d3971b8220fd4
binay94 <binaya.vicky@gmail.com>
2023-05-19 15:56:33 +0530

    first commit

--- user_cred.py
+++ user_cred.py
@@ -1,3 +1,3 @@
 username = "" # MongoDB Username
 password = "" # MongoDB Password
-file_path = "" # Enter File Path to Store Course Details in pdf
+file_path = "A:\\ineuron_scrapper\\Courses_pdf\\" # Enter File Path to Store Course Details in pdf
